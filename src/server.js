f1b191b988d4653b3f17307f0743c37fd3735180
DeVeom <deveomm@gmail.com>
2021-10-25 19:06:15 +0900

    Modify: server.js - import http, server 변수 추가하여 서버 실행

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
+import http from 'http';
 import dotenv from 'dotenv';
 import app from './app';
+
+const server = http.createServer(app);
 
 dotenv.config();
 const { PORT } = process.env;
 
 const start = async () => {
   try {
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`OUR CODE TRIP Backend server is listening on ${PORT}!`);
     });
   } catch (err) {
